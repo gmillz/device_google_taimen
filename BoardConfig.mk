LineageOS/android_device_google_taimen

taimen: Declare kernel defconfig
Change-Id: Iaecc1f0ee5c23359266fb0db12897b71b4536609

BoardConfig.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Allow Lineage config to override others
+-include device/google/taimen/BoardConfigLineage.mk
+
 TARGET_BOOTLOADER_BOARD_NAME := taimen
 DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS := 0x0000008c
 

gmillz/device_google_taimen

BoardConfig.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Allow Lineage config to override others
+-include device/google/taimen/BoardConfigSlim.mk
+
 TARGET_BOOTLOADER_BOARD_NAME := taimen
 DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS := 0x0000008c
 